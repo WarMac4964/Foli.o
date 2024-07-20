93ab429b21e229341fb4b6fdadd44131303fd9cb
Anurag Tyagi <anurag.tyagi4964@gmail.com>
2024-07-20 19:24:15 +0530

    init: project setup

---
+++ Foli.o/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foli.o
+//
+//  Created by Anurag Tyagi on 20/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
